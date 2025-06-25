a3e685d7626e82b3bd979dcda4454c3f7d143b85
Kireeti <kireeti@ggwp.com>
2025-06-25 16:58:50 +0530

    Some more updates to patient policy

--- cami/policy/patient_policy.py
+++ cami/policy/patient_policy.py
@@ -2,13 +2,13 @@ import datetime
 
 from pydantic import BaseModel, Field
 
-from .policy import Policy
-
 
 class PatientPolicy(BaseModel):
     patient_id: str = Field(..., title="Patient ID")
     policy_id: str = Field(..., title="Policy ID purchased by the patient")
-    date_of_purchase: datetime.datetime = Field(..., title="Date of purchase")
+    date_of_purchase: datetime.datetime = Field(..., title="Purchase date of the policy")
+    valid_till: datetime.datetime = Field(..., title="Date till the policy is valid")
+    sum_insured: int = Field(..., title="Patient sum insured")
 
     def to_dict(self) -> dict:
         return self.model_dump()
